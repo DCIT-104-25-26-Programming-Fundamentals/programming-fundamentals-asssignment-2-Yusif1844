bb8ce5b1e2c5e9943e4d6e1ba0b4ca739e51e8ed
Abdul Samed Yusif <haskayyusuf22@gmail.com>
2026-07-31 03:41:16 -0700

    Implement prime number checker function

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -36,4 +36,48 @@
 
 #include <iostream>
 using namespace std;
+#include <iostream>
+using namespace std;
+
+// Returns true if n is a prime number, false otherwise.
+bool isPrime(int n) {
+    // Numbers less than 2 are never prime.
+    if (n < 2) {
+        return false;
+    }
+
+    // 2 is the only even prime number.
+    if (n == 2) {
+        return true;
+    }
+
+    // Eliminate other even numbers quickly.
+    if (n % 2 == 0) {
+        return false;
+    }
+
+    // Only need to check odd divisors up to sqrt(n).
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (isPrime(number)) {
+        cout << number << " is a prime number." << endl;
+    } else {
+        cout << number << " is NOT a prime number." << endl;
+    }
+
+    return 0;
+}
 
